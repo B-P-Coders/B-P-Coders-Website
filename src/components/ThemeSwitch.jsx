8577d98bff5d36933e59596c4ac189e5b84f1ffd
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-07 23:47:15 +0200

    Zacząłem navbar

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -7,7 +7,7 @@ export default function ThemeSwitch()
 {
     const [isDark, setIsDark] = useState(false);
     return(
-        <div className="switch" onClick={() => setIsDark(!isDark)}>
+        <div className={styles.main} onClick={() => setIsDark(!isDark)}>
             <i className={isDark ? "fas fa-sun" : "fas fa-moon"}></i>
             <link rel="stylesheet" href={isDark ? "/themes/vardark.css" : "/themes/varlight.css"} />
             <link rel="stylesheet" href="/themes/var.css" />
