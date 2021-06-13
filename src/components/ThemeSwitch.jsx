d4057b5ea329a1b84217ca1aca4acc97dc611bfe
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-13 19:45:44 +0200

    Dodano zapisywanie motywu w ciasteczkach

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { useState } from "react"
+import { useCookies } from "react-cookie"
 
 import * as styles from "../styles/ThemeSwitch.module.scss"
 
 export default function ThemeSwitch()
 {
-    const [isDark, setIsDark] = useState(false);
+    const [cookies, setCookie, removeCookie] = useCookies(["theme"]);
+    const [isDark, setIsDark] = useState(cookies["theme"]);
     return(
-        <div className={styles.main} onClick={() => setIsDark(!isDark)}>
+        <div className={styles.main} onClick={() => {setCookie("theme", !isDark); setIsDark(!isDark)}}>
             <i className={isDark ? "fas fa-sun" : "fas fa-moon"}></i>
             <link rel="stylesheet" href={isDark ? "/themes/vardark.css" : "/themes/varlight.css"} />
             <link rel="stylesheet" href="/themes/var.css" />
