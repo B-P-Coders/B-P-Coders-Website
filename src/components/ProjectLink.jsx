65e58f50e5d3f1250a9610c68d42cf9ec12d2c85
bart016 <bart.turczynski@gmail.com>
2021-06-15 21:53:05 +0200

    project link
    style i tekst

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -6,13 +6,12 @@ export default function ProjectLink({ name, link, img })
 {
     return (
         <li>
-            <a href={link}>
-                <img src={img} alt={name} />
+            <a href={link} className="image">
+                <img src={img} alt={name}  />
+                </a>
                 <h4>{name}
-                
-                
+                COVID Clicker jest pierwszym wspólnym projektem naszej grupy B&P Coders, wykonywany był on głównie w maju 2021r. Jego tematyką jest COVID-19, a celem wyklikanie dziennej liczby zakażeń odnotowanych danego dnia w Polsce.
                 </h4>
-            </a>
         </li>
     )
 }
