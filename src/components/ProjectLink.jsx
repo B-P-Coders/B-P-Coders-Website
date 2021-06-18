f438c37494d94bdc7a12cc9f6d1493b843839add
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-18 21:19:03 +0200

    Poprawiono komponent ProjectLink

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import * as styles from "../styles/ProjectLink.scss"
 
-export default function ProjectLink({ name, link, img })
+export default function ProjectLink({ name, link, img, desc})
 {
     return (
         <li>
@@ -10,7 +10,7 @@ export default function ProjectLink({ name, link, img })
                 <img src={img} alt={name}  />
                 </a>
                 <h4 className="text" >{name}
-                COVID Clicker jest pierwszym wspólnym projektem naszej grupy B&P Coders, wykonywany był on głównie w maju 2021r. Jego tematyką jest COVID-19, a celem wyklikanie dziennej liczby zakażeń odnotowanych danego dnia w Polsce.
+                {desc}
                 </h4>
         </li>
     )
