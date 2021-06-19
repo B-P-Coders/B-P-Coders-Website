a39d674ec6a3c5d9edf51b8e538643a36d5db7f9
bart016 <bart.turczynski@gmail.com>
2021-06-19 20:47:12 +0200

    o nas - wstęp

--- src/pages/o-nas.jsx
+++ src/pages/o-nas.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import Layout from "../components/layout"
 import * as styles from "../styles/o-nas.module.scss"
+import Iframe from 'react-iframe'
+
 
 export default function ONas()
 {
     return(
-        <Layout>
-            
-        </Layout>
+       <Layout>
+         <p className={styles.naglowek}>Główny ośrodek naszego zespołu</p>
+<Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d551.4014904769929!2d20.703833253768355!3d49.610601806104306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd28d18fb32dc47f8!2sZesp%C3%B3%C5%82%20Szk%C3%B3%C5%82%20Elektryczno-Mechanicznych%20im.%20gen.%20J%C3%B3zefa%20Kustronia!5e0!3m2!1spl!2spl!4v1624127039723!5m2!1spl!2spl"
+        width="600px"
+        height="450px"
+        display="initial"
+        position="relative"
+        style="border:0;" allowfullscreen="" 
+        />
+        <p className={styles.naglowek}> Kim jesteśmy?</p>
+        </Layout> 
     )
 }
