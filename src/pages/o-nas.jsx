934fe76a54a6777f710efb277dca291b043194f9
bart016 <bart.turczynski@gmail.com>
2021-06-21 21:50:13 +0200

    o nas -ciąg dalszy

--- src/pages/o-nas.jsx
+++ src/pages/o-nas.jsx
@@ -10,13 +10,18 @@ export default function ONas()
        <Layout>
          <p className={styles.naglowek}>Główny ośrodek naszego zespołu</p>
 <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d551.4014904769929!2d20.703833253768355!3d49.610601806104306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd28d18fb32dc47f8!2sZesp%C3%B3%C5%82%20Szk%C3%B3%C5%82%20Elektryczno-Mechanicznych%20im.%20gen.%20J%C3%B3zefa%20Kustronia!5e0!3m2!1spl!2spl!4v1624127039723!5m2!1spl!2spl"
-        width="600px"
+        width="700px"
         height="450px"
         display="initial"
-        position="relative"
-        style="border:0;" allowfullscreen="" 
+         allowfullscreen=""
+        className={styles.ifr} 
         />
-        <p className={styles.naglowek}> Kim jesteśmy?</p>
+        <p  style={{backgroundColor: "#3eadcf"
+}} className={styles.text}>       <br></br>  <span  className={styles.naglowek}> Kim jesteśmy? </span> <br></br> <br></br>
+B&P Coders to grupa programistów chcących swoją pracą w dziale IT zmieniać świat na lepsze. Działalność naszej organizacji rozpoczęła się nieformalnie już w 2021r. napędzana pragnieniem samorozwoju i zbudowania czegoś większego. 
+Później nasze ambicje i osiągnięcia jedynie rosły... 
+        </p>
+
         </Layout> 
     )
 }
