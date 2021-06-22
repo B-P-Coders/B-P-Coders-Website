b595e205c20b19bac48f77be2907f3479f2df96c
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-22 20:41:24 +0200

    Dodałem formularz zamówień

---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import * as styles from "../styles/OrderForm.module.scss"
+
+export default function OrderForm()
+{
+    return(
+        <div className={styles.border}>
+            <form netlify={true} className={styles.main}>
+                <h1>Złóż zamówienie</h1>
+                <input required type="text" name="name" placeholder="Imię i nazwisko"/>
+                <br />
+                <input required type="email" name="email" placeholder="E-mail"/>
+                <br />
+                <textarea required name="description" placeholder="Opisz swoje zamówienie" rows="10" cols="30"></textarea>
+                <br />
+                <input type="submit" value="Wyślij"/>
+            </form>
+        </div>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import AnimatedLogo from "../components/AnimatedLogo"
 import Quote from "../components/Quote"
+import OrderForm from "../components/OrderForm"
 
 import * as styles from "../styles/index.module.scss"
 
@@ -21,6 +22,9 @@ export default function Index()
             </section>
             <section id="quote" className={styles.quote}>
                 <Quote/>
+            </section>
+            <section id="order" className={styles.order}>
+                <OrderForm/>
             </section>
         </Layout>
     )
