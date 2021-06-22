73c2749a36a247fb945fbd80c893d8cd51178bf4
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-22 12:09:03 +0200

    Poprawiono config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Oficjalna strona B&P Coders`,
+        short_name: `B&P Coders`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
