7d1296f9dd64a6d46e28bd11649077f4a0910fbc
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-25 21:37:50 +0200

    Poprawiłem link do clickera

--- src/pages/projekty.jsx
+++ src/pages/projekty.jsx
@@ -11,7 +11,7 @@ export default function Projekt()
         <Layout>
             <h1>Nasze pojekty:</h1>
             <ul>
-                <ProjectLink link="https://covid-clicker.netlify.app/" img={covidBanner} desc="COVID Clicker jest pierwszym wspólnym projektem naszej grupy B&P Coders, wykonywany był on głównie w maju 2021r. Jego tematyką jest COVID-19, a celem wyklikanie dziennej liczby zakażeń odnotowanych danego dnia w Polsce."/>
+                <ProjectLink link="https://covidclicker.ml" img={covidBanner} desc="COVID Clicker jest pierwszym wspólnym projektem naszej grupy B&P Coders, wykonywany był on głównie w maju 2021r. Jego tematyką jest COVID-19, a celem wyklikanie dziennej liczby zakażeń odnotowanych danego dnia w Polsce."/>
             </ul>
         </Layout>
     )
