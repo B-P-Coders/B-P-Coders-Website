d4534d76a21c91956f45d54f92450850de907356
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-25 13:04:18 +0200

    Poprawka formularza v2

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -6,8 +6,9 @@ export default function OrderForm()
 {
     return(
         <div className={styles.border}>
-            <form data-netlify="true" className={styles.main}>
+            <form data-netlify="true" data-netlify-honeypot="bot-field" className={styles.main}>
                 <h1>Złóż zamówienie</h1>
+                <input type="hidden" name="form-name" value="order"/>
                 <input required type="text" name="name" placeholder="Imię i nazwisko"/>
                 <br />
                 <input required type="email" name="email" placeholder="E-mail"/>
