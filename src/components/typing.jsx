9fc1b5e465fa8b16fd7005f5b5bf58e89dcbb027
n00sek <n00sek@onet.pl>
2021-07-14 20:20:08 +0200

    taka pierdola, npm install ityped

---
+++ src/components/typing.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped';
+import * as styles from "../styles/typing.scss"
+
+export default function Typing() {
+
+    const ityped = useRef(0);
+
+    useEffect(() => {
+        if (!ityped.current) {
+          return;
+        }
+        init(ityped.current, { 
+          showCursor: false, 
+          strings: ['B&P Coders:',  'Bartosz Mółka',  'Bartosz Turczyński',  'Piotr Nowakowski',  'Paweł Floryan'],
+          backDelay: 1000 
+        })
+      }, []);
+    return (
+        <h1 className={styles.cursor}><span ref={ityped}></span></h1>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-
+import Typing from "../components/typing"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import AnimatedLogo from "../components/AnimatedLogo"
 import Quote from "../components/Quote"
 import OrderForm from "../components/OrderForm"
-
 import * as styles from "../styles/index.module.scss"
 
 export default function Index()
@@ -17,7 +16,7 @@ export default function Index()
             <section id="main" className={styles.main}>
                 <AnimatedLogo/>
                 <div className={styles.title}>
-                    <h1>B&P Coders</h1>
+                    <Typing/>
                 </div>
             </section>
             <section id="quote" className={styles.quote}>
