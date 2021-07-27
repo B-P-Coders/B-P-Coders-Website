02c4a02b8a43ff35cecef6d5eaedb3c52caf50bb
bart016 <bart.turczynski@gmail.com>
2021-07-27 21:33:00 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <Helmet>
+            <meta name="google-site-verification" content="reQeyS_IYC8aF7QgDwsR35wfPJuiz2nAetOufv3NT6s" />
                 <meta charset="utf-8" />
                 <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
