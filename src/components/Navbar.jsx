c0a5cd724d9cac864c48f25948c32e0b241cc605
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-07-31 10:44:51 +0200

    Baner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,10 @@ export default function Navbar() {
             <Helmet>
             <meta name="google-site-verification" content="reQeyS_IYC8aF7QgDwsR35wfPJuiz2nAetOufv3NT6s" />
                 <meta charset="utf-8" />
-                <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#000000" />
-                <meta name="description" content="Oficjalna strona B&P Coders" />
-                <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-                <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+                <link rel="apple-touch-icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
                     integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
                     crossorigin="anonymous" />
@@ -29,17 +27,16 @@ export default function Navbar() {
                 <meta property="description" content="Oficjalna strona internetowa studia programistycznego B&P Coders" />
                 <meta property="twitter:title" content="B&P Studio" />
                 <meta property="title:description" content="Oficjalna strona B&P Coders" />
-                <meta property="og:image" content="covid-clicker-banner.png" />
-                <meta property="twitter:image" content="covid-clicker-banner.png" />
+                <meta property="og:image" content="/bpcoders-banner.jpg" />
+                <meta property="twitter:image" content="/bpcoders-banner.jpg" />
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="shortcut icon" content="/favicon.ico" />
                 <meta property="apple icon" content="/favicon.ico" />
-                <meta name="description" content="Oficjalna strona B&P Coders" />
                 <meta property="og:title" content="B&P Studio" />
                 <meta property="og:description" content="Oficjalna strona B&P Coders" />
                 <meta property="og:type" content="website" />
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:creator" content="@piotreknow02 &amp; @OreoPL-Studio &amp; @bart016 &amp; @n00sek" />
+                <meta name="twitter:creator" content="@B&amp;P Coders"/>
                 <meta name="twitter:title" content="B&P Studio" />
                 <meta name="twitter:description" content="Oficjalna strona B&P Coders" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
