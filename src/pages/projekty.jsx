9f35cf6cb3568443f2beec7f3e9e3167e45caea8
bart016 <bart.turczynski@gmail.com>
2021-08-17 12:23:45 +0200

    nowe projekty

--- src/pages/projekty.jsx
+++ src/pages/projekty.jsx
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import ProjectLink from "../components/ProjectLink"
 import * as styles from "../styles/projekty.module.scss"
 import covidBanner from "../images/covid-clicker-banner.png"
+import textige from "../images/Logo.svg"
+import chodza from "../images/banner.webp"
 
 export default function Projekt()
 {
@@ -12,6 +14,10 @@ export default function Projekt()
             <h1>Nasze pojekty:</h1>
             <ul className={styles.ul}>
                 <ProjectLink link="https://covidclicker.ml" img={covidBanner} desc="COVID Clicker jest pierwszym wspólnym projektem naszej grupy B&P Coders, wykonywany był on głównie w maju 2021r. Jego tematyką jest COVID-19, a celem wyklikanie dziennej liczby zakażeń odnotowanych danego dnia w Polsce."/>
+                <ProjectLink link="https://textige.ml" img={textige} desc="Strona w budowie...."/>
+                <ProjectLink link="https://chodzasluchy.ml" img={chodza} desc="Strona przygotowana dla klasy 2C w I LO w Nowym Sączu. Był to pierwszy dochodowy projekt B&P Coders, który pomógł rozwinąć się dziennikarsko innym uczniom."/>
+
+     
             </ul>
         </Layout>
     )
